375fbd3cf8844dddc5049abc1222dc0d2f2f98ac
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-10-06 10:45:53 +0100

    Added a new JS file - creating a basic weather app

---
+++ script2.js
@@ -0,0 +1,9 @@
+// New JavaScript Activity
+
+/*
+The methods document.querySelector is how javascript "talk" to an html document,
+it then uses a method .querySelector to select html tags, so we have a select and paragraph tag in our
+document and this will allow us to to access their information/values
+*/
+const select = document.querySelector("select");
+const paragraph = document.querySelector("p");
