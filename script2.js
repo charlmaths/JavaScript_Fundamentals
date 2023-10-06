0c2812a6a3ff7f9fc0ff299d7a8df8c16ede1141
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-10-06 11:03:42 +0100

    Created a simple JS function using some html doc queries

--- script2.js
+++ script2.js
@@ -1,9 +1,38 @@
 // New JavaScript Activity
+/*
+First is we declare a variable using 'const' and we called these variables 'select' and 'paragraph'
+*/
+const select = document.querySelector("select");
+const paragraph = document.querySelector("p");
 
 /*
 The methods document.querySelector is how javascript "talk" to an html document,
 it then uses a method .querySelector to select html tags, so we have a select and paragraph tag in our
 document and this will allow us to to access their information/values
 */
-const select = document.querySelector("select");
-const paragraph = document.querySelector("p");
+
+select.addEventListener("change", setWeather);
+/*
+This is called an event listener, basically this will listen to the <select> element in the HTML document
+not in this JS script - and essentially if the value in <select> is changed, we can trigger the function below
+the "change" is the event - we need this event listener to trigger our js function
+*/
+
+function setWeather(){
+    // whatever the value we get from select we are saving it into a new variable called choice
+    // then we will write the conditionals below:
+    const choice = select.value;
+
+    // .textContent is how javascript outputs stuff onto an HTML document - we will study this further later
+    if (choice === "sunny") {
+      paragraph.textContent = "It is sunny - go outside and swim";
+    } else if (choice === "rainy") {
+      paragraph.textContent = "It is rainy - go outside and bring an umbrella";
+    } else if (choice === "snowy") {
+      paragraph.textContent = "It is snowy - go outside and bring a thick jacket";
+    } else if (choice === "cloudy") {
+      paragraph.textContent = "It is cloudy - go outside and bring a light jacket";
+    } else {
+        paragraph.textContent = ""; // This will be empty because before we select anything, its empty
+    }
+}
