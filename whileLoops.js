209f0f2b4ff09cc6cce0897467184598fcaae1fb
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-10-17 09:37:15 +0100

    Added while loop file

---
+++ whileLoops.js
@@ -0,0 +1,26 @@
+// While loops in J.S.
+// We always need an initialiser, condition, and increment
+/*
+Pseudo code would look like this:
+
+initialiser
+
+while (condition) {
+    some code here
+
+    increment here
+}
+*/
+
+// example 1 - repeat word 5 times
+
+const wordRepeat = "Repeat this"
+
+i = 1
+
+while (i < 5) {
+    console.log(wordRepeat)
+    i++;
+}
+
+
