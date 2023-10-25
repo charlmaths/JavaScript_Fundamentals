4c4a671b7359b5be6c1dd326323491ef8a75e8c1
Charles Martinez <gat-x131@Charless-MacBook-Pro.local>
2023-10-25 10:02:58 +0100

    Created for loop example

---
+++ forLoops.js
@@ -0,0 +1,11 @@
+// some for loops exercise using JS
+// asking user using prompt function to enter a number then we loop it until we reach the number
+
+// save the value into a variable - we must using parseInt to convert string to integer
+let answer = parseInt(5);
+
+// for loop will have 3 inputs essentially - initialiser, condition, increment all are OPTIONAL
+
+for (let i = 1; i <= answer; i++) {
+  console.log("This will loop until: ", i);
+}
